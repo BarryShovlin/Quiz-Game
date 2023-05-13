7e1f2dab70e4c01851df05eaaae6989b2e52dd85
BarryShovlin <bshovlin32@gmail.com>
2023-05-12 23:09:20 -0500

    created question class. created question bank with question objects

---
+++ question_model.py
@@ -0,0 +1,9 @@
+class Question:
+
+    def __init__(self, text, answer):
+        self.text = text
+        self.answer = answer
+
+
+# question = Question("Blue bird is blue", "True")
+# print(question.text, question.answer)
