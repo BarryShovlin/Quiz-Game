ca7ad1692d8f9cfc848552123ff9edb7b9a67cce
BarryShovlin <bshovlin32@gmail.com>
2023-05-13 21:50:44 -0500

    new questions in question bank, updated main.py to use new format of question data

--- data.py
+++ data.py
@@ -1,51 +1,80 @@
 question_data = [
     {
-        "category": "Entertainment: Music",
+        "category": "Science: Mathematics",
         "type": "boolean",
         "difficulty": "easy",
-        "question": "The music group Daft Punk got their name from a negative review they recieved.",
+        "question": "A universal set, or a set that contains all sets, exists.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "111,111,111 x 111,111,111 = 12,345,678,987,654,321",
         "correct_answer": "True", "incorrect_answers": ["False"]
     },
     {
-        "category": "Entertainment: Music", "type": "boolean", "difficulty": "easy",
-        "question": "Eurobeat is primarily an Italian genre of music.", "correct_answer": "True",
-        "incorrect_answers": ["False"]
-    },
-    {
-        "category": "Entertainment: Music", "type": "boolean", "difficulty": "easy",
-        "question": "The song &quot;Stronger Than You&quot; is a single by Estelle, who played Garnet in Steven Universe.",
-        "correct_answer": "True", "incorrect_answers": ["False"]
-    },
-    {
-        "category": "Entertainment: Music", "type": "boolean", "difficulty": "easy",
-        "question": "A Saxophone is a brass instrument.", "correct_answer": "False", "incorrect_answers": ["True"]
-    },
-    {
-        "category": "Entertainment: Music", "type": "boolean", "difficulty": "easy",
-        "question": "Daft Punk originated in France.", "correct_answer": "True", "incorrect_answers": ["False"]
-    },
-    {
-        "category": "Entertainment: Music", "type": "boolean", "difficulty": "easy",
-        "question": "Scatman John&#039;s real name was John Paul Larkin.", "correct_answer": "True",
-        "incorrect_answers": ["False"]
-    },
-    {
-        "category": "Entertainment: Music", "type": "boolean", "difficulty": "easy",
-        "question": "Michael Jackson wrote The Simpsons song &quot;Do the Bartman&quot;.",
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "Zero factorial is equal to zero. ",
         "correct_answer": "False", "incorrect_answers": ["True"]
     },
     {
-        "category": "Entertainment: Music", "type": "boolean", "difficulty": "easy",
-        "question": "The 2011 movie &quot;The Adventures of Tintin&quot; was directed by Steven Spielberg.",
-        "correct_answer": "True", "incorrect_answers": ["False"]
-    },
-    {
-        "category": "Entertainment: Music", "type": "boolean", "difficulty": "easy",
-        "question": "John Williams composed the music for &quot;Star Wars&quot;.", "correct_answer": "True",
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "A &#039;Millinillion&#039; is a real number.",
+        "correct_answer": "True",
         "incorrect_answers": ["False"]
     },
     {
-        "category": "Entertainment: Music", "type": "boolean", "difficulty": "easy",
-        "question": "Lead Singer Rivers Cuomo of American rock band Weezer attended Harvard.",
-        "correct_answer": "True", "incorrect_answers": ["False"]
-    }]
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "hard",
+        "question": "In Topology, the complement of an open set is a closed set.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "The set of all algebraic numbers is countable.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "hard",
+        "question": "L&#039;H&ocirc;pital was the mathematician who created the homonymous rule that uses derivatives to evaluate limits with indeterminations.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "The sum of any two odd integers is odd.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "medium",
+        "question": "You can square root a negative number with an imaginary number &quot;i&quot;.",
+        "correct_answer": "True",
+        "incorrect_answers": ["False"]
+    },
+    {
+        "category": "Science: Mathematics",
+        "type": "boolean",
+        "difficulty": "easy",
+        "question": "A scalene triangle has two sides of equal length.",
+        "correct_answer": "False",
+        "incorrect_answers": ["True"]
+    }
+]
